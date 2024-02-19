c8bdb45a90e697fd976569debd1802679238e4e6
Tyler Clark <tylerc111@gmail.com>
2024-02-19 15:06:25 -0500

    added initial functionality to Category.js, ProductTag.js, and Tag.js files

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,20 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [1, 30]
+      }
+    }
   },
   {
     sequelize,
