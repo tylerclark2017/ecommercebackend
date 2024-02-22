cd69b30a68fa6aae05de7ecb99009958db800392
Tyler Clark <tylerc111@gmail.com>
2024-02-22 10:14:49 -0500

    added foreign key relationships in index.js file

--- models/index.js
+++ models/index.js
@@ -6,22 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'XXXXX',
+  foreignKey: 'category_id',
 });
 // Categories have many Products
 Category.hasMany(Product,
   {
-    foreignKey: 'XXXXX',
+    foreignKey: 'category_id',
   });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,
   {
     through: ProductTag,
+    foreignKey: 'product_id',
   })
   // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,
   {
     through: ProductTag,
+    foreignKey: 'tag_id',
   })
 
 
